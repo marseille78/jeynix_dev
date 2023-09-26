5d9954645715ff63e227f2c4fba9a15ad7a38fec
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-09-26 20:47:36 +0300

    Overfill content

--- dev/main.js
+++ dev/main.js
@@ -47,6 +47,8 @@ document.addEventListener('DOMContentLoaded', function() {
   ***********************************/
   
   (function () {
+    const mainContainer = document.querySelector(".page__main");
+
     const pageSlider = new Swiper("[data-swiper]", {
       // Свої класи
       wrapperClass: "slider__wrapper",
@@ -122,6 +124,50 @@ document.addEventListener('DOMContentLoaded', function() {
         // Можливість перетягувати скролл
         draggable: true,
       },
+
+      // Відключення автоініціалізації
+      init: false,
+
+      // Події
+      on: {
+        // Подія ініціалізації
+        init: function() {
+          setScrollType();
+          mainContainer.classList.add("loaded");
+        },
+
+        // Подія зміни слайда
+        slideChange: function() {},
+
+        // Зміна розміру вікна браузера
+        resize: function() {
+          setScrollType();
+        }
+      },
     });
+
+    function setScrollType() {
+      if (mainContainer.classList.contains("free")) {
+        mainContainer.classList.remove("free");
+        pageSlider.params.freeMode = false;
+      }
+
+      for (let i = 0; i < pageSlider.slides.length; i++) {
+        const itemSlide = pageSlider.slides[i];
+        const itemSlideContent = itemSlide.querySelector(".section__content");
+
+        if (itemSlideContent) {
+          const itemSlideContentHeight = itemSlideContent.offsetHeight;
+
+          if (itemSlideContentHeight > window.innerHeight) {
+            mainContainer.classList.add("free");
+            pageSlider.params.freeMode = true;
+            break;
+          }
+        }
+      }
+    }
+
+    pageSlider.init();
   })();
 });
